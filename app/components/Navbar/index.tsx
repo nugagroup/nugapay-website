54d29ac88339e783ac99be5b896ed07198a24c9e
Sunday Etom Eni <sundaywht@gmail.com>
2025-07-22 10:03:13 +0100

    updated the app download link

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../shared/Button";
 import logo from "@/app/assets/logo.png";
-// import { downloadApp } from "../shared/helper";
-import { dashboardLink } from "../shared/helper";
+import { downloadApp } from "../shared/helper";
+// import { dashboardLink } from "../shared/helper";
 
 export const Navbar = () => {
 
@@ -26,8 +26,8 @@ export const Navbar = () => {
           />
         </Link>
         
-        {/* <Button onClick={downloadApp} variant="primary" className="!min-h-10">Get the App</Button> */}
-        <Button onClick={dashboardLink} variant="primary" className="!min-h-10">Get Started</Button>
+        <Button onClick={downloadApp} variant="primary" className="!min-h-10">Get the App</Button>
+        {/* <Button onClick={dashboardLink} variant="primary" className="!min-h-10">Get Started</Button> */}
       </nav>
       <div className="w-full h-px bg-gradient-to-l from-[#474F9A] to-[#D6DAFF]"></div>
     </header>
